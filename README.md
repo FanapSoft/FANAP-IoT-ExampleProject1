1b8907f0af4f62a9fa974368e311713bb74762a5
Abd Manian <a.manian@fanap.ir>
2019-02-25 17:42:33 +0330

    Add new type blinker module

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FANAP-IoT-ExampleProject1
 Example Arduino Project for using Fanap IoT Platform
+
+## Requirements
+- ESP32 development board
+- Arduino IDE (tested with v1.8.8)
+- Libraries:
+  - WiFi
+  - ArduinoJson
+  - PubSubClient
---
+++ src/ledblinker.h
@@ -0,0 +1,60 @@
+#ifndef _LED_BLINKER_H
+#define _LED_BLINKER_H
+
+#include "Arduino.h"
+
+class LedBlinker
+{
+public:
+
+    void init(int led_pin) {
+        pin = led_pin;
+        pinMode(pin, OUTPUT);
+    }
+
+    void setstate(int* statelist) {
+        this->statelist = statelist;
+        index  = 0;
+        blink_time = millis();
+        apply_led();
+    }
+
+    void handle() {
+        unsigned long diff = millis() - blink_time;
+
+        if (diff>get_cur_led_state()) {
+            next_state();
+            blink_time = millis();
+            apply_led();
+        }
+    }
+
+private:
+    int pin;
+    int * statelist;
+    int index;
+    unsigned long blink_time;
+
+
+    void apply_led() {
+        digitalWrite(pin, (statelist[index]>0)?1:0);
+    }
+
+    int get_cur_led_state() {
+        int c = statelist[index];
+        if (c<0) {
+            c=-c;
+        }
+        return c;
+    }
+
+    void next_state() {
+        index++;
+        if (!statelist[index])  {
+            index = 0;
+        }
+    }
+};
+
+
+#endif
